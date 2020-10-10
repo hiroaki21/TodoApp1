dd120093a3dbb1e317ac66e42550d84f4b0a9653
hiroaki21 <k.hi0201t.mkksk@gmail.com>
2020-10-10 22:14:45 +0900

    テーブルビュー画面作成

--- TodoApp1/ViewController.swift
+++ TodoApp1/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
 
+    @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
