7333d5e7f42f3b96a099a162d1338bd67547eeed
hiroaki21 <k.hi0201t.mkksk@gmail.com>
2020-10-10 22:29:05 +0900

    tableViewに関するメソッド準備

--- TodoApp1/ViewController.swift
+++ TodoApp1/ViewController.swift
@@ -16,18 +16,30 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        
+        textField.delegate = self
     }
 
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         <#code#>
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return view.frame.size.height/6
+    }
 
 }
 
